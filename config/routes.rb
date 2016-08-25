d1722b7ac01fec6b720be840dc58110ed9bd0543
wakazoku <wakazoku@gmail.com>
2016-08-26 04:20:27 +0900

    Chapter5 リソースについて

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as:"about"
   get "lesson/:action(/:name)" => "lesson"
+
+  resources :members do
+  	collection { get "search" }
+  end
 end
