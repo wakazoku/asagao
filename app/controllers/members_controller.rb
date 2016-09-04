c6108cf625dcdc0690d8cfe1cc09fb6631357883
wakazoku <wakazoku@gmail.com>
2016-09-04 22:40:02 +0900

    バリデーション、エラーメッセージの表示まで。 todo:なんかgemファイルがインストール出来ないのでSSL証明書等を確認する

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -15,18 +15,38 @@ class MembersController < ApplicationController
 		@member = Member.find(params[:id])
 	end
 
+	# 新規作成フォーム
 	def new
+		@member = Member.new(birthday: Date.new(1980, 1, 1))
 	end
 
+	# 更新フォーム
 	def edit
+		@member = Member.find(params[:id])
 	end
 
 	def create
+		@member = Member.new(params[:member])
+		if @member.save
+			redirect_to @member, notice: "会員情報を登録しました"
+		else
+			render "new"
+		end
 	end
 
 	def update
+		@member = Member.find(params[:id])
+		@member.assign_attributes(params[:member])
+		if @member.save
+			redirect_to @member, notice: "会員情報を更新しました"
+		else
+			render "edit"
+		end
 	end
 
 	def destroy
+		@member = Member.find(params[:id])
+		@member.destroy
+		redirect_to :members, notice: "会員を削除しました" 
 	end
 end
