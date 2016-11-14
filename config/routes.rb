7234225fd4211a088d1b5771bd1ab05d7dfcd8c7
wakazoku <wakazoku@gmail.com>
2016-11-14 22:34:43 +0900

    8-4 マイアカウントページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   get "about" => "top#about", as:"about"
   get "lesson/:action(/:name)" => "lesson"
 
+  # membersモデルをリソースとして扱う。
+  # collectionとしてsearchメソッドを使う
   resources :members do
   	collection { get "search" }
   end
   resources :articles
   resource :session, only: [:create, :destroy]
+  # accountモデルをリソースとして扱うのは後述のコントローラに記載されているメソッドのみ
+  resource :account, only: [:show, :edit, :update]
 end
